69fda5a153e7085054e87cb5af1e52643e51ba24
Ayush Sharma <ayushsharma170199@gmail.com>
2025-01-16 10:13:26 +0530

    elsint config at root level

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+import { ESLint } from "eslint";
+
+export default new ESLint({
+  baseConfig: {
+    env: {
+      browser: true,
+      es2020: true,
+    },
+    extends: [
+      "eslint:recommended",
+      "plugin:react/recommended",
+      "plugin:@typescript-eslint/recommended",
+      "prettier",
+    ],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+      ecmaVersion: 11,
+      sourceType: "module",
+    },
+    plugins: ["react", "@typescript-eslint"],
+    rules: {
+      // Add your custom rules here
+    },
+  },
+});
